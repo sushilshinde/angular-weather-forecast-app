ceede821ff4e5619434ab4146f89953337a99ea5
nadaf-msys <nsharapoddin@msystechnologies.com>
2023-08-11 14:26:44 +0530

    Changed project name to repo name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-app'`, () => {
+  it(`should have as title 'angular-weather-forecast-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.title).toEqual('angular-weather-forecast-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-weather-forecast-app app is running!'
+    );
   });
 });
